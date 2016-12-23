65cf601b86328624f9f85b20e11efc208062a04b
hbc <bcxxxxxx@gmail.com>
2016-12-23 19:32:41 +0800

    doc(readme): update installation guide

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="bearychat",
-    version="0.1",
+    version="0.1.0",
     description="SDK for BearyChat",
     url="bearychat.com",
     install_requires=[

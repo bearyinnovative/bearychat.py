5de0af365ab62718a5e2f2efb9413bc4a0857f3e
hbc <bcxxxxxx@gmail.com>
2017-05-12 11:37:15 +0800

    test(openapi): add testcases

--- tests/openapi/test_client.py
+++ tests/openapi/test_client.py
@@ -1,6 +1,6 @@
 import pytest
 
-from bearychat.openapi.client import Requester
+from bearychat.openapi import client
 
 
 _build_url_testcases = [
@@ -17,5 +17,41 @@ _build_url_testcases = [
 
 @pytest.mark.parametrize('base_url,api_method,expected', _build_url_testcases)
 def test_requester___build_url(base_url, api_method, expected):
-    requester = Requester(base_url)
+    requester = client.Requester(base_url)
     assert requester._build_url(api_method) == expected
+
+
+format_method_testcases = [
+    ('foo', 'bar', 'foo.bar'),
+    ('foo', None, 'foo'),
+]
+
+
+@pytest.mark.parametrize('service_name,method_name,expected',
+                         format_method_testcases)
+def test_format_method(service_name, method_name, expected):
+    assert client.format_method(service_name, method_name) == expected
+
+
+def test_get_api():
+    assert client.get_api('meta', None)
+    assert client.get_api('channel', 'list')
+
+    with pytest.raises(RuntimeError):
+        client.get_api('foobar', None)
+
+    with pytest.raises(RuntimeError):
+        client.get_api('channel', 'foobar')
+
+
+def test_get_methods():
+    assert client.get_methods('meta') == ['meta']
+    assert len(client.get_methods('channel')) > 0
+
+    with pytest.raises(RuntimeError):
+        client.get_methods('foobar')
+
+
+def test_client_meta():
+    c = client.Client('foobar')
+    assert c.meta(), 'should return meta'

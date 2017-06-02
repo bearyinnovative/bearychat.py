ebab423bde51225e91dfe949f657f7a0140edfdd
hbc <bcxxxxxx@gmail.com>
2017-06-02 08:38:16 +0800

    chore: bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(join(CURRENT_DIR, "README.rst")) as long_description_file:
 
 setup(
     name="bearychat",
-    version="0.3.0",
+    version="0.4.0",
     author="bearyinnovative",
     url="https://github.com/bearyinnovative/bearychat.py",
     description="SDK for BearyChat",
